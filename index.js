22a7c7049ccf8fbc1fd79663ef16f172c7fcdb4b
Daniel Dawson <dannyldawson@gmail.com>
2018-11-10 00:55:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   var newObject = Object.assign({}, obj, { [key] : value});
   return newObject;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromObjectByKey(obj, key) {
+  var newObject = Object.assign({}, obj);
+  delete newObject[key];
+  return newObject;
+}
